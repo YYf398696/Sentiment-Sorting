c92587febb76c289268cba66074980da5d7959fb
YYf398696 <70981017+YYf398696@users.noreply.github.com>
2020-10-29 11:51:25 +0800

    Add files via upload

--- best_and_worst_business_idea.py
+++ best_and_worst_business_idea.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
-import spacy
-nlp = spacy.load("en_core_web_lg")
 
+import nltk
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+Data = pd.read_csv('MergedCompanies.csv')
+analyser = SentimentIntensityAnalyzer()
+def sentiment_analysis(text):
+    return analyser.polarity_scores(text)
+
+for sen in Data:
+    print(sen)
+    ss = analyser.polarity_scores(sen)
+    for k in ss:
+        print('{0}:{1},'.format(k, ss[k]), end='')
+    print()
 
